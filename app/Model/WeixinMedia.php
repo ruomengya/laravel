95e61a3f88fd1852d3c8eab75876df37f978403c
root <root@localhost.localdomain>
2019-02-21 03:01:56 +0000

    Wx

---
+++ app/Model/WeixinMedia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WeixinUser extends Model
+{
+    public $table = 'wx_media';
+    //    public $timestamps = false;
+}
---
+++ app/Model/WeixinUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WeixinModel extends Model
+{
+    public $table = 'wx_users';
+    //    public $timestamps = false;
+}
